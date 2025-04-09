7fb2195dea7221ea61785c6ac01ed7e33cfa4afb
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-04-09 05:19:44 -0300

    Update build.sh

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,49 +1,49 @@
-PKG_VER="10.1-8-esync-xinput"
+PKG_VER="proton_3.16"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
+
 BLACKLIST_ARCHITECTURE=aarch64
 
-GIT_URL="https://github.com/GloriousEggroll/wine-ge-custom.git"
-GIT_COMMIT="8aebc458014a5fd4741d83711ae812493aef7ef2"
-
+GIT_URL=https://github.com/kakra/wine-proton.git
+GIT_COMMIT=df1bf897eeefac0215cdf823e3f7dc51e66cb31d
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
 OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
-
 CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
-                --host=$TOOLCHAIN_TRIPLE \
-                --with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
-                --prefix=$OVERRIDE_PREFIX \
-                --without-oss \
-                --disable-winemenubuilder \
-                --disable-win16 \
-                --disable-tests \
-                --with-x \
-                --x-libraries=$PREFIX/lib \
-                --x-includes=$PREFIX/include \
-                --with-pulse \
-                --with-gstreamer \
-                --with-opengl \
-                --with-gnutls \
-                --with-mingw=gcc \
-                --with-xinput \
-                --with-xinput2 \
-                --enable-nls \
-                --without-xshm \
-                --without-xxf86vm \
-                --without-osmesa \
-                --without-usb \
-                --without-sdl \
-                --without-cups \
-                --without-netapi \
-                --without-pcap \
-                --without-gphoto \
-                --without-v4l2 \
-                --without-pcsclite \
-                --without-wayland \
-                --without-opencl \
-                --without-dbus \
-                --without-sane \
-                --without-udev \
-                --without-capi"
+				--host=$TOOLCHAIN_TRIPLE \
+				--with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
+				--prefix=$OVERRIDE_PREFIX \
+				--without-oss \
+				--disable-winemenubuilder \
+				--disable-win16 \
+				--disable-tests \
+				--with-x \
+				--x-libraries=$PREFIX/lib \
+				--x-includes=$PREFIX/include \
+				--with-pulse \
+				--with-gstreamer \
+				--with-opengl \
+				--with-gnutls \
+				--with-mingw=gcc \
+				--with-xinput \
+				--with-xinput2 \
+    				--enable-nls \
+				--without-xshm \
+				--without-xxf86vm \
+				--without-osmesa \
+				--without-usb \
+				--without-sdl \
+				--without-cups \
+				--without-netapi \
+				--without-pcap \
+				--without-gphoto \
+				--without-v4l2 \
+				--without-pcsclite \
+				--without-wayland \
+				--without-opencl \
+				--without-dbus \
+				--without-sane \
+				--without-udev \
+				--without-capi"
+    
