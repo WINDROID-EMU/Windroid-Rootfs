5ad8ac28a4d6d83c24ea699e1b1525e58061d3cb
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2025-04-09 04:57:25 -0300

    Wine próton teste

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -1,48 +1,49 @@
 PKG_VER="10.1-8-esync-xinput"
 PKG_CATEGORY="Wine"
 PKG_PRETTY_NAME="Wine ($PKG_VER)"
-
 BLACKLIST_ARCHITECTURE=aarch64
 
-GIT_URL=https://github.com/KreitinnSoftware/wine
-GIT_COMMIT=7e93e231f5b27f59f06bd9fbd72705ea7ddccd1a
+GIT_URL="https://github.com/GloriousEggroll/wine-ge-custom.git"
+GIT_COMMIT="GE-Proton7-15"
+
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
 OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
+
 CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
-				--host=$TOOLCHAIN_TRIPLE \
-				--with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
-				--prefix=$OVERRIDE_PREFIX \
-				--without-oss \
-				--disable-winemenubuilder \
-				--disable-win16 \
-				--disable-tests \
-				--with-x \
-				--x-libraries=$PREFIX/lib \
-				--x-includes=$PREFIX/include \
-				--with-pulse \
-				--with-gstreamer \
-				--with-opengl \
-				--with-gnutls \
-				--with-mingw=gcc \
-				--with-xinput \
-				--with-xinput2 \
-    				--enable-nls \
-				--without-xshm \
-				--without-xxf86vm \
-				--without-osmesa \
-				--without-usb \
-				--without-sdl \
-				--without-cups \
-				--without-netapi \
-				--without-pcap \
-				--without-gphoto \
-				--without-v4l2 \
-				--without-pcsclite \
-				--without-wayland \
-				--without-opencl \
-				--without-dbus \
-				--without-sane \
-				--without-udev \
-				--without-capi"
+                --host=$TOOLCHAIN_TRIPLE \
+                --with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
+                --prefix=$OVERRIDE_PREFIX \
+                --without-oss \
+                --disable-winemenubuilder \
+                --disable-win16 \
+                --disable-tests \
+                --with-x \
+                --x-libraries=$PREFIX/lib \
+                --x-includes=$PREFIX/include \
+                --with-pulse \
+                --with-gstreamer \
+                --with-opengl \
+                --with-gnutls \
+                --with-mingw=gcc \
+                --with-xinput \
+                --with-xinput2 \
+                --enable-nls \
+                --without-xshm \
+                --without-xxf86vm \
+                --without-osmesa \
+                --without-usb \
+                --without-sdl \
+                --without-cups \
+                --without-netapi \
+                --without-pcap \
+                --without-gphoto \
+                --without-v4l2 \
+                --without-pcsclite \
+                --without-wayland \
+                --without-opencl \
+                --without-dbus \
+                --without-sane \
+                --without-udev \
+                --without-capi"
