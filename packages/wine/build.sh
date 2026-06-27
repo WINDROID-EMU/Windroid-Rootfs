79f16de76b43939b3f587acf0c976f656330bb8f
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-06-27 18:37:25 -0300

    Wine Esync
     Desabilitei a delayed mutex write optimization que estava causando o loop infinito.

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=6c6fad36e3c825cb003e13a0c14217d4a509f7b5
+GIT_COMMIT=3c90588ae5717c3d0ab83ca22f4a4b14589a500b
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
