d546bb253d737a73bafa1202588adbd51d6cea69
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-07-21 17:29:10 -0300

    Update build script to remove deprecated option
    Removed '-Daalib=disabled' from MESON_ARGS as it is no longer recognized in this version.

--- packages/gst-plugins-good/build.sh
+++ packages/gst-plugins-good/build.sh
@@ -1,6 +1,7 @@
 PKG_VER=1.29.2
 SRC_URL=https://gstreamer.freedesktop.org/src/gst-plugins-good/gst-plugins-good-$PKG_VER.tar.xz
 MESON_ARGS="-Dcairo=disabled -Dexamples=disabled -Dgdk-pixbuf=disabled -Doss=disabled "
-MESON_ARGS+="-Doss4=disabled -Dtests=disabled -Dv4l2=disabled -Daalib=disabled"
+# Removido -Daalib=disabled pois a opção não é mais reconhecida nesta versão
+MESON_ARGS+="-Doss4=disabled -Dtests=disabled -Dv4l2=disabled"
 LDFLAGS="-L$PREFIX/lib -landroid-shmem"
 DEPENDENCIES="glib gst-plugins-base android-shmem"
