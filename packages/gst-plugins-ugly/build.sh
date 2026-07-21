952dd0343d947bf3cf646f3c3409f84a3a5b0045
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-07-21 07:56:58 -0500

    Packages gstreamer update

--- packages/gst-plugins-ugly/build.sh
+++ packages/gst-plugins-ugly/build.sh
@@ -1,4 +1,4 @@
-PKG_VER=1.24.11
+PKG_VER=1.29.2
 SRC_URL=https://gstreamer.freedesktop.org/src/gst-plugins-ugly/gst-plugins-ugly-$PKG_VER.tar.xz
 MESON_ARGS="-Dtests=disabled"
 DEPENDENCIES="glib gst-plugins-base"
