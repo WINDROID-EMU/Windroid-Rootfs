9f65e9fda18975dd7f9fa3b4ae47380f9637d65a
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-07-29 01:57:04 -0500

    Add Patch 004
    d3dx9

--- packages/wine/build.sh
+++ packages/wine/build.sh
@@ -6,7 +6,7 @@ PKG_DOWNLOADABLE=1
 BLACKLIST_ARCH=aarch64
 
 GIT_URL=https://github.com/WINDROID-EMU/Windroid-Wine
-GIT_COMMIT=b92968786702cfa5eb6f55b1c8968ec18a724f46
+#GIT_COMMIT=b92968786702cfa5eb6f55b1c8968ec18a724f46
 HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
 HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
 HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
