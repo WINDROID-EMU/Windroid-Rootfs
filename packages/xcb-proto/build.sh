bed251d2114be10653360cd6211a4bf8e231b7b3
WINDROID-EMU <45230455+WINDROID-EMU@users.noreply.github.com>
2026-07-29 04:08:07 -0500

    Troca de  link xcb-proto Package

--- packages/xcb-proto/build.sh
+++ packages/xcb-proto/build.sh
@@ -1,3 +1,3 @@
 PKG_VER=1.17.0
-SRC_URL=https://xorg.freedesktop.org/archive/individual/proto/xcb-proto-$PKG_VER.tar.xz
+SRC_URL=https://sources.voidlinux.org/xcb-proto-$PKG_VER/xcb-proto-$PKG_VER.tar.xz
 CONFIGURE_ARGS=" "
