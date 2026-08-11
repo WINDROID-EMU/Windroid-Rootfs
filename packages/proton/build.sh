aebc7c3d3acb47442b010852f73901c1321f4fe8
Miceano <pablolabs36@gmail.com>
2026-08-10 21:06:27 -0500

    Update libX11 and xorgproto build scripts, add proton package

---
+++ packages/proton/build.sh
@@ -0,0 +1,51 @@
+PKG_VER="Proton"
+PKG_CATEGORY="Wine"
+PKG_PRETTY_NAME="Wine ($PKG_VER)"
+PKG_DOWNLOADABLE=1
+
+BLACKLIST_ARCH=aarch64
+
+GIT_URL=https://github.com/WINDROID-EMU/Windroid-Proton
+#GIT_COMMIT=b92968786702cfa5eb6f55b1c8968ec18a724f46
+HOST_BUILD_CONFIGURE_ARGS="--enable-win64 --without-x"
+HOST_BUILD_FOLDER="$INIT_DIR/workdir/$package/wine-tools"
+HOST_BUILD_MAKE="make -j $(nproc) __tooldeps__ nls/all"
+OVERRIDE_PREFIX="$(realpath $PREFIX/../wine)"
+CONFIGURE_ARGS="--enable-archs=i386,x86_64 \
+				--host=$TOOLCHAIN_TRIPLE \
+				--with-wine-tools=$INIT_DIR/workdir/$package/wine-tools \
+				--prefix=$OVERRIDE_PREFIX \
+				--without-oss \
+				--disable-winemenubuilder \
+				--disable-win16 \
+				--disable-tests \
+				--with-x \
+				--x-libraries=$PREFIX/lib \
+				--x-includes=$PREFIX/include \
+				--with-pulse \
+				--with-gstreamer \
+				--with-opengl \
+				--with-gnutls \
+				--with-mingw=gcc \
+				--with-xinput \
+				--with-xinput2 \
+				--enable-nls \
+				--without-xshm \
+				--without-xxf86vm \
+				--without-osmesa \
+				--without-usb \
+				--without-sdl \
+				--without-cups \
+				--without-netapi \
+				--without-pcap \
+				--without-gphoto \
+				--without-v4l2 \
+				--without-pcsclite \
+				--without-wayland \
+				--without-opencl \
+				--without-dbus \
+				--without-sane \
+				--without-udev \
+				--without-capi"
+
+DEPENDENCIES="libX11 libXext libXcomposite libXrender libXcursor libXrandr libXxf86vm libXinerama libXfixes libXi Vulkan-Headers Vulkan-Loader libglvnd pulseaudio freetype libgnutls gstreamer gst-plugins-base gst-plugins-ugly gst-plugins-good gst-plugins-bad cabextract"
