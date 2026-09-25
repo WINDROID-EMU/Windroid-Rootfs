2e26bfe5e59598f3951c42387ce802e41a704eef
genrogamer <genrogamer@gmail.com>
2026-09-25 03:40:58 -0300

    packages: remove obsolete mesa-demos and FreeGLUT (X11/GLX demos)

--- packages/FreeGLUT/build.sh
+++
@@ -1,6 +0,0 @@
-PKG_VER=3.4.0
-SRC_URL=https://downloads.sourceforge.net/freeglut/freeglut-$PKG_VER.tar.gz
-CMAKE_ARGS="-DANDROID=OFF -DCMAKE_LIBRARY_PATH=$PREFIX_DIR/lib"
-CFLAGS="-I$PREFIX_DIR/include"
-LDFLAGS="-L$PREFIX_DIR/lib -lGL -landroid-shmem"
-DEPENDENCIES="libX11 libXi libXrandr libXxf86vm GLU android-shmem"
